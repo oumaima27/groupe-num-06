5a8064185d65af0f20cbc0cc08fe3f409c228306
oumaima maghraoui <99296158+oumaima27@users.noreply.github.com>
2022-02-09 20:18:25 +0100

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,12 @@
+fonction f1(){
+ 		let rayon=prompt("saisir rayon")
+ 		const pi=3,14;
+ 		var p=pi*2*rayon;
+ 		p=p to fixed(2);
+ 		alert(p);
+ 		console.log(p);
+ 		document.getelementbid(p1).innerHTML=p;
+ 	}
+
+
+ 	
